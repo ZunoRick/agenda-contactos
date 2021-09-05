71802e120d713ab5e2633a0d2703ec74d3288b23
ZunoRick <zunosan.ricardo506@gmail.com>
2021-09-04 19:57:47 -0500

    configurando la base de datos

--- inc/funciones/bd.php
+++ inc/funciones/bd.php
@@ -1,11 +1,11 @@
 <?php 
 
 //Credenciales de la base de datos
-define('DB_USUARIO', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_HOST', 'localhost');
-define('DB_PORT', '3310');
-define('DB_NAME', 'agendaphp');
+define('DB_USUARIO', 'epiz_29623928');
+define('DB_PASSWORD', 'WJWwBbeLFjuy');
+define('DB_HOST', 'sql101.epizy.com');
+define('DB_PORT', '3306');
+define('DB_NAME', 'epiz_29623928_agendaphp');
 
 $conn = new mysqli(DB_HOST, DB_USUARIO, DB_PASSWORD, DB_NAME, DB_PORT);
 
